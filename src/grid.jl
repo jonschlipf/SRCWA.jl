dadf547e2bedd47c4edd11c79a3dc5cb1cb6b47e
jonschlipf <schlipf.jon@gmx.net>
2019-06-28 11:03:19 +0200

    fixed instability with complex type

--- src/grid.jl
+++ src/grid.jl
@@ -21,9 +21,9 @@ end
 function grid_k(nx,ny,theta,alpha,lambda,ax,ay,epsilon_ref)
     #straightforward
     #all k vectors are generally normalized to k0 here
-    k0=2*pi/lambda
+    k0=2*pi/real(lambda)
     #The incoming wave, transformed from spherical coordinates to normalized cartesian coordinates, |kin|=1
-    kin=[sin(theta*pi/180)*cos(alpha*pi/180),sin(theta*pi/180)*sin(alpha*pi/180),cos(theta*pi/180)]*sqrt(epsilon_ref)
+    kin=[sin(theta*pi/180)*cos(alpha*pi/180),sin(theta*pi/180)*sin(alpha*pi/180),cos(theta*pi/180)]*real(sqrt(epsilon_ref))
     #the spatial vectors are the sum of the incoming vector and the reciprocal lattice vectors
     kx=kin[1].+(2*pi/ax)*nx/k0;
     ky=kin[2].+(2*pi/ay)*ny/k0;
